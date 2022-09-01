494fa49e405c84a8bc777e6970b15ed6df6cd653
guumuller <gustavomuller160305@gmail.com>
2022-09-01 12:07:50 -0300

    gugu
    gugu

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Lojinha</title>
+
+<header id="header">
+    <h1 id="headerh1">Lojinha</h1>
+<ul id="navbar" class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Ativo</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+</ul>
+</header>
+<main>
+
+</main>
+<footer>
+
+</footer>
+</html>
